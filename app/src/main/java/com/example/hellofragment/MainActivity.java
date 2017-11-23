f9d06378dc9a4b2e7bf6196a0482e77972e65209
jasonMisaq <powergame_coder2@yahoo.com>
2017-11-23 21:20:34 +0330

    Added A timer with sound

--- app/src/main/java/com/example/hellofragment/MainActivity.java
+++ app/src/main/java/com/example/hellofragment/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.hellofragment;
 
+import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mTime= 60;
+    private TextView mTimeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.fragment_container,fragment);
         ft.commit();
+        mTimeTV = findViewById(R.id.button);
+        Handler handler = new Handler();
+        Timer timer = new Timer();
 
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (mTime > 0) {
+                    mTime--;
+                }
+                else {
+                    mTime = 60; //reset
+                }
+                //Only the main thread can touch its views
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTimeTV.setText(String.valueOf(mTime));
+
+                    }
+                });
+
+            }
+        };
+        timer.schedule(timerTask,1000,1000);
     }
 }
